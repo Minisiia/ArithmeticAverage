ffabc3b129a19b4baad52bfa89a4ac8fe775ebf3
Minisiia <ar_tik@yahoo.com>
2022-11-20 17:46:12 +0200

    Java Start. 3. Data Types and Variables. Arithmetic Operators

---
+++ src/ArithmeticAverage.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArithmeticAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть а");
+        double a = scanner.nextDouble();
+        System.out.println("Введіть b");
+        double b = scanner.nextDouble();
+        System.out.println("Введіть c");
+        double c = scanner.nextDouble();
+        double average = (a + b + c) / 3;
+        System.out.println("Середнє арифметичне " + average);
+//        для цілочисельних змінних підходить тип int.
+//        але через те, що середнє арифметичне може бути дійсним числом, то і
+//        для запису цілочисельних змінних необхідно використовувати тип double
+    }
+}
